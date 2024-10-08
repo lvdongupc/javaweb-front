84c545fb7960e8eed272ed22d9e9b6db62e52871
ldd <2567783488@qq.com>
2024-10-08 11:44:45 +0800

    javaweb

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!-- <h1>{{ message }}</h1> -->
+    <!-- <element-view></element-view> -->
+    <!-- <emp-view></emp-view> -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      message: "Hello"
+    }
+  },
+}
+</script>
+<style></style>
---
+++ src/views/element/ElementView.vue
@@ -0,0 +1,95 @@
+<template>
+    <div>
+        <!-- button按钮 -->
+        <el-row>
+            <el-button>默认按钮</el-button>
+            <el-button type="primary">主要按钮</el-button>
+            <el-button type="success">成功按钮</el-button>
+            <el-button type="info">信息按钮</el-button>
+            <el-button type="warning">警告按钮</el-button>
+            <el-button type="danger">危险按钮</el-button>
+        </el-row>
+        <br>
+        <br>
+
+
+        <!-- Table表格 -->
+        <el-table :data="tableData" border style="width: 100%">
+            <el-table-column prop="date" label="日期" width="180"></el-table-column>
+            <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+            <el-table-column prop="address" label="地址"></el-table-column>
+        </el-table>
+
+        <br>
+        <br>
+
+        <!-- Pagination分页 -->
+        <el-pagination background layout="prev, pager, next" :total="1000"></el-pagination>
+
+
+        <br>
+        <br>
+        <!-- Dialog对话框 -->
+        <el-button type="text" @click="dialogTableVisible = true">打开嵌套表格的 Dialog</el-button>
+
+        <el-dialog title="收货地址" :visible.sync="dialogTableVisible">
+            <el-table :data="gridData">
+                <el-table-column property="date" label="日期" width="150"></el-table-column>
+                <el-table-column property="name" label="姓名" width="200"></el-table-column>
+                <el-table-column property="address" label="地址"></el-table-column>
+            </el-table>
+        </el-dialog>
+
+        <br>
+        <br>
+
+
+        <!-- Form表单 -->
+
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            gridData: [{
+                date: '2016-05-02',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            }, {
+                date: '2016-05-04',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            }, {
+                date: '2016-05-01',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            }, {
+                date: '2016-05-03',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            }],
+            dialogTableVisible: false,
+            tableData: [{
+                date: '2016-05-02',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            }, {
+                date: '2016-05-04',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1517 弄'
+            }, {
+                date: '2016-05-01',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1519 弄'
+            }, {
+                date: '2016-05-03',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1516 弄'
+            }]
+        }
+    },
+}
+</script>
+
+<style></style>
